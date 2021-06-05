a9dd12a7f8eb615a6d70ac7bcfb4b9e787eb0ed9
ceng408 <85250248+ceng408@users.noreply.github.com>
2021-06-05 15:10:12 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {Link } from "react-router-dom";
+import logo from './Pages/images/Logo.png';
+import hosgeldiniz from './Pages/images/hosgeldiniz.png';
+import './App.css';
+function App() {
+  return (
+    
+    <div className="Table"  >
+     
+      <table className="Table_m2" >
+        <thead >
+        <tr>
+          <th className="th_logo"><Link to="/"><img src={logo} alt="Logo" /></Link></th>
+        </tr>
+        <tr>
+          <th></th>
+        </tr>
+        <tr>
+          <th className="th_hosgeldiniz"><text>hosgeldinizhosgeldinizhosgeldin</text><img  src={hosgeldiniz} alt="logo" /></th>
+        </tr>
+        <tr>
+          <th></th>
+        </tr>
+        <tr>
+          <th><Link to="/page3"><button className="btn-1">Metinden Görüntüye Çeviri </button> </Link></th>
+        </tr>
+        <tr>
+          <th><br></br></th>
+        </tr>
+        <tr>
+        <th><Link to="/page2"><button className="btn-1">Görüntüden Metine Çeviri </button> </Link></th>
+        </tr>
+       </thead>
+    </table>
+  </div>
+  );
+  
+}
+export default App;
---
+++ src/Pages/utilities.js
@@ -0,0 +1,33 @@
+// Define our labelmap
+const labelMap = {
+    1: {'name':'Güzel', color:'red'},
+    2: {'name':'Evet', color:'yellow'},
+    3: {'name':'Sıkılmak', color:'pink'},
+    4: {'name':'Cevap', color:'blue'},
+    5: {'name':'Seni Seviyorum', color:'purple'},
+    6: {'name':'Bayan', color:'orange'},
+    7: {'name':'Beş', color:'green'}
+}
+
+// Define a drawing function
+export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx)=>{
+    for(let i=0; i<=boxes.length; i++){
+        if(boxes[i] && classes[i] && scores[i]>threshold){
+            // Extract variables
+            const [y,x,height,width] = boxes[i]
+            const text = classes[i]
+            
+            // Set styling
+            ctx.strokeStyle = labelMap[text]['color']
+            ctx.lineWidth = 10
+            ctx.fillStyle = 'white'
+            ctx.font = '30px Arial'         
+            
+            // DRAW!!
+            ctx.beginPath()
+            ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
+            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/1.5);
+            ctx.stroke()
+        }
+    }
+}
